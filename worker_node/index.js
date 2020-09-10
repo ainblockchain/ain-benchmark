e4c96b902e1c2ace8ca7ec719e2e2d2589ee51e9
csh <csh@comcom.ai>
2020-09-10 19:32:07 +0900

    Use 'PORT' environment variable

--- worker_node/index.js
+++ worker_node/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const worker = require('./worker');
 
 app.use(express.json());
