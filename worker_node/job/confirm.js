4bf268b3eb4894facc1c61e820ef359f08138fd1
csh <csh@comcom.ai>
2020-09-11 18:08:22 +0900

    Implement confirm

--- worker_node/job/confirm.js
+++ worker_node/job/confirm.js
@@ -1,9 +1,54 @@
 const Base = require('./base');
+const Ain = require('@ainblockchain/ain-js').default;
 
 class Confirm extends Base {
-  // TODO(sanghee) : Implement
-  constructor() {
-    super();
+  static configProps = [
+    'ainUrl',
+    'startBlockNumber',
+    'finishBlockNumber',
+    'txHashList',
+  ];
+  #ain;
+
+  constructor(config) {
+    super(config, Confirm.configProps);
+    this.output = {
+      message: '',
+      statistics: {
+        tps: null,
+        lossRate: null, // TODO(sanghee)
+      },
+    };
+    this.#ain = new Ain(this.config.ainUrl);
+    this.#ain.provider.setDefaultTimeoutMs(60 * 1000);
+  }
+
+  async requestTxHashList(from, to) {
+    const txHashList = [];
+    for (let number = from; number <= to; number++) {
+      const block = await this.#ain.getBlock(number, true);
+      txHashList.push(...block.transactions.map(it => it.hash));
+    }
+    return txHashList;
+  }
+
+  async calculateDuration(from, to) {
+    const startTime = (await this.#ain.getBlock(from)).timestamp;
+    const finishTime = (await this.#ain.getBlock(to)).timestamp;
+    return finishTime - startTime; // ms
+  }
+
+  async process() {
+    const startBlockNumber = this.config.startBlockNumber;
+    const finishBlockNumber = this.config.finishBlockNumber;
+    const txHashListInRange = await this.requestTxHashList(startBlockNumber, finishBlockNumber);
+    const durationInRange = await this.calculateDuration(startBlockNumber, finishBlockNumber);
+    const tps = txHashListInRange.length / (durationInRange / 1000);
+
+    this.output.statistics.tps = tps;
+    this.output.statistics.durationInRange = durationInRange;
+
+    return this.output;
   }
 }
 
