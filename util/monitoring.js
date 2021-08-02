5e06b362d1dc38b5c0f1540b5eb047ca7cf9ec67
pengin7384 <pengin7384@gmail.com>
2021-08-02 11:43:54 +0900

    Remove instanceName from getMemory* functions

--- util/monitoring.js
+++ util/monitoring.js
@@ -75,7 +75,7 @@ async function getNetworkReceivedInfo(client, projectId, instanceName, startTime
   return requestAndAssembleInfo(client, request);
 }
 
-async function getMemoryUsageInfo(client, projectId, instanceName, startTime, endTime) {
+async function getMemoryUsageInfo(client, projectId, startTime, endTime) {
   const filter = `metric.type="agent.googleapis.com/memory/percent_used" AND metric.labels.state="used"`
   const request = makeRequest(client, projectId, filter, startTime, endTime);
   request.aggregation = {
@@ -87,7 +87,7 @@ async function getMemoryUsageInfo(client, projectId, instanceName, startTime, en
   return requestAndAssembleInfo(client, request);
 }
 
-async function getMemoryBytesInfo(client, projectId, instanceName, startTime, endTime) {
+async function getMemoryBytesInfo(client, projectId, startTime, endTime) {
   const filter = `metric.type="agent.googleapis.com/memory/bytes_used" AND metric.labels.state="used"`
   const request = makeRequest(client, projectId, filter, startTime, endTime);
   request.aggregation = {
@@ -113,8 +113,8 @@ async function getMonitoringInfoFromGoogleCloud(projectId, instanceName, keyFile
   info.network.incoming = await getNetworkReceivedInfo(monitoringClient, projectId, instanceName, startTime, endTime);
   info.network.outgoing = await getNetworkSentInfo(monitoringClient, projectId, instanceName, startTime, endTime);
   info.memory = {};
-  info.memory.usage = await getMemoryUsageInfo(monitoringClient, projectId, instanceName, startTime, endTime);
-  info.memory.bytes_used = await getMemoryBytesInfo(monitoringClient, projectId, instanceName, startTime, endTime);
+  info.memory.usage = await getMemoryUsageInfo(monitoringClient, projectId, startTime, endTime);
+  info.memory.bytes_used = await getMemoryBytesInfo(monitoringClient, projectId, startTime, endTime);
   return info;
 }
 
